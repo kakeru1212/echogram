b34f0bfcfb4da929043f025411f1b63eaf3058c8
kakeru1212 <2000kakeru0307@gmail.com>
2025-05-14 19:30:44 +0900

    サインアウト処理を修正。

--- components/ui/signout-button.tsx
+++ components/ui/signout-button.tsx
@@ -1,13 +1,19 @@
-'use client'
+'use client';
 
-import { FiLogOut } from 'react-icons/fi'
+import { FiLogOut } from 'react-icons/fi';
 
 export const SignOutButton = () => {
-
+  const base = 'https://master.dww3cxr5fnjzb.amplifyapp.com';
   return (
-    <button onClick={() => window.location.href = '/api/auth/logout'} className="flex items-center">
+    <button
+      onClick={() =>
+        window.location.href =
+          `/api/auth/logout?federated&returnTo=${encodeURIComponent(base)}`
+      }
+      className="flex items-center"
+    >
       <FiLogOut className="mr-2" />
       サインアウト
     </button>
-  )
-}
+  );
+};
